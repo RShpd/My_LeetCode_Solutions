6535224aa0ec257bcbb4fe9991ce853b45d8c252
Shahpasand <ra.shahpasand@gmail.com>
2021-12-05 17:46:49 +0330

    Update ImplementStrSolution.java

--- ImplementStrSolution.java
+++ ImplementStrSolution.java
@@ -1,18 +1,31 @@
+//Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+//Level: easy
+
+//Runtime: 1591 ms   It's the time when I saw the impact of an on time return!
+//Memory Usage: 39.3 MB
+
 public class ImplementStrSolution {
     public static int strStr(String haystack, String needle) {
         if (needle.isEmpty()) return 0;
-        int index = 0;
+        if (haystack.isEmpty() || haystack.length() < needle.length()) return -1;
         int i = 0;
-        while(index<haystack.length() && i<needle.length()){
-            if(haystack.charAt(index++) != needle.charAt(i++)){
-                i = 0;
+
+        while (i < haystack.length()) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                int j;
+                for (j = 0; j < needle.length(); j++) {
+                    if (haystack.length() <= i + j) return -1;  //if you replace this return statement with a break, you will encounter a time limit exceed
+                    if (haystack.charAt(i + j) != needle.charAt(j)) break;
+                }
+                if (j == needle.length()) return i;
             }
+            i++;
         }
-        return (i == needle.length()) ? index-i : -1;
+        return -1;
     }
 
     public static void main(String args[]) {
-        int result = strStr("hello", "ll");
+        int result = strStr("mississippi", "issipi");
         System.out.println(result);
     }
 }
