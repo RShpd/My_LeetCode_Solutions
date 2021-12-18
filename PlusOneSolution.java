53896f0f93daca0c8999d4ef3614a71bafd1bcc5
Shahpasand <ra.shahpasand@gmail.com>
2021-12-18 23:23:31 +0330

    Update PlusOneSolution.java

--- PlusOneSolution.java
+++ PlusOneSolution.java
@@ -1,26 +1,30 @@
+//You are given a large integer represented as an integer array digits,
+// where each digits[i] is the ith digit of the integer.
+//Level: easy
+
+//Runtime: 0ms
+//Memory:  37.3 MB
+
 public class PlusOneSolution {
     private static int[] plusOne(int[] digits) {
-        int lastIndex = digits.length - 1;
-        if (digits[lastIndex] < 9) {
-            digits[lastIndex] = digits[lastIndex] + 1;
-        } else {
-            int[] newArray = new int[digits.length + 1];
-            for (int i = newArray.length-1; i > 0; i--) {
-                if (digits[i] == 9) {
-                    newArray[i] = 0;
-                    newArray[i-1] = 1;
-                } else {
-                    newArray[i] = digits[i];
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i] = (digits[i] + 1) % 10;
+            if (digits[i] > 0) break;
+            if(i==0 && digits[i] == 0){
+                int[] newArray = new int[digits.length + 1];
+                newArray[0] = 1;
+                int j = 1;
+                for (int n: digits) {
+                    newArray[j++] = n;
                 }
-
+                return newArray;
             }
-            digits = newArray;
         }
         return digits;
     }
 
     public static void main(String[] args) {
-        int[] testCase = {9,9,9};
+        int[] testCase = {8, 9, 9};
         int[] result = plusOne(testCase);
         System.out.println(result.length);
     }
